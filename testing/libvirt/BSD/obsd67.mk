6bbf5637f0fafbebb2b84bc07a98aa1e9346f263
Ravi Teja <13203149+RaviTejaCMS@users.noreply.github.com>
2020-08-14 17:38:54 +0530

    Resolving errors

--- testing/libvirt/BSD/obsd67.mk
+++ testing/libvirt/BSD/obsd67.mk
@@ -21,30 +21,30 @@ endef
 define kvm-obsd-create
 	if [[ -f "$(KVM_POOLDIR)/$(KVM_BSD_ISO)" ]]; then \
 	    echo "$(KVM_BSD_ISO) exists in Pool directory" \ 
-	else 
-	    echo "ISO does not exist Dowloading the file"
-		wget --output-document $KVM_BSD_ISO.tmp --no-clobber -- $(KVM_ISO_URL_BSD) -P $(KVM_POOLDIR)
-		mv $KVM_BSD_ISO.tmp $KVM_BSD_ISO
-	fi
-	if [[ -z $(call kvm-clean-obsd $(KVM_BSD_BASE_NAME)) ]]
-	then
-	   echo "VM does not exist "
-	   $(call kvm-base-obsd)
-	else
-	    echo "VM is running!"
-		echo "Undefining VM"
-	fi
+	else \
+	    echo "ISO does not exist Dowloading the file" \
+		wget --output-document $KVM_BSD_ISO.tmp --no-clobber -- $(KVM_ISO_URL_BSD) -P $(KVM_POOLDIR) \
+		mv $KVM_BSD_ISO.tmp $KVM_BSD_ISO \
+	fi \
+	if [[ -z $(call kvm-clean-obsd $(KVM_BSD_BASE_NAME)) ]] \
+	then \
+	   echo "VM does not exist " \
+	   $(call kvm-base-obsd) \
+	else \
+	    echo "VM is running!" \
+		echo "Undefining VM" \
+	fi \
 endef
 define kvm-base-obsd
-		sed -e "s:@@TESTINGDIR@@:$$(KVM_TESTINGDIR):" rc.firsttime > $(KVM_POOLDIR)/rc.firsttime
-		cp install.conf boot.conf $(KVM_POOLDIR)/
+		sed -e "s:@@TESTINGDIR@@:$$(KVM_TESTINGDIR):" rc.firsttime > $(KVM_POOLDIR)/rc.firsttime \
+		cp install.conf boot.conf $(KVM_POOLDIR)/ \
 		sudo env -i growisofs -M "$(KVM_POOLDIR)/install67.iso" -l -R -graft-points /install.conf="$(KVM_POOLDIR)/install.conf" /etc/boot.conf="$(KVM_POOLDIR)/boot.conf" /rc.firsttime="$(KVM_POOLDIR)/rc.firsttime"
 		sudo virt-install --name=$(KVM_BSD_BASE_NAME) --virt-type=kvm --memory=2048,maxmemory=2048 \
         --vcpus=1,maxvcpus=1 --cpu host --os-variant=$(VIRT_BSD_VARIANT) \
         --cdrom=$(KVM_POOLDIR)/install67.iso \
         $(VIRT_GATEWAY) \
         --disk path=$(KVM_POOLDIR)/$(KVM_BSD_BASE_NAME).qcow2,size=4,bus=virtio,format=qcow2 \
-        --graphics none --serial pty
-		$(KVM_PYTHON) $(KVM_TESTINGDIR)/utils/obsdinstall.py $(KVM_BSD_BASE_NAME)
+        --graphics none --serial pty \
+		$(KVM_PYTHON) $(KVM_TESTINGDIR)/utils/obsdinstall.py $(KVM_BSD_BASE_NAME) \
 	#This should be continued for east and wet too to write that also sed for east and west
 endef
