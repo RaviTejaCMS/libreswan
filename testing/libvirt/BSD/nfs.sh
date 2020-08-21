7b9154f5f66ecd2adde9517ed4a0aa94f0a01268
Ravi Teja <13203149+RaviTejaCMS@users.noreply.github.com>
2020-08-21 16:39:57 +0530

    Fixed some errors

--- testing/libvirt/BSD/nfs.sh
+++ testing/libvirt/BSD/nfs.sh
@@ -4,6 +4,6 @@ if grep -qF "192.1.2.0/24" /etc/exports;then
    echo "fstab entry already exists"
 else
    echo "@@TESTINGDIR@@ 192.1.2.0/24(rw,no_root_squash)"
-   sudo systemctl start nfs-server
 fi
-exportfs -r
+sudo systemctl start nfs-server
+sudo exportfs -r
